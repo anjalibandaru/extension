a4ef3535e898ed7a5ba03004336ae24297f8a0e6
anjalibandaru <anjalibandaru513@gmail.com>
2017-02-15 22:29:06 +0530

    first commit

---
+++ dbhome.php
@@ -0,0 +1,20 @@
+
+ 
+ <div class="menu">
+ <?php include 'header.html';?>
+ </div>
+  <main class="masthead">
+ 	<div class="container">
+ 		<center><img class="ilead" src="http://www.pngall.com/wp-content/uploads/2016/05/MySQL-Logo.png" width=200px height=150px></center>
+ 		<p class="blead">
+ 			<strong>Struggling to find a MySQL DBA?</strong>
+ 		</p>
+ 		<p class="slead">
+ 			<strong>Outsource database to us.</strong><br>We provide 24x7 support to your database.
+ 		</p>
+	 	<p class=blead>
+	 		<a href="#" class="btn btn-outline-inverse btn-lg contactus" onclick="ga("send","event","Jumbotron actions")">CONTACT US</a>
+	 	</p>
+	</div>
+	</main>
+ 
---
+++ dbs1.php
@@ -0,0 +1,35 @@
+<title>Flexible Consulating</title>
+<div class="menu">
+ <?php include 'header.html';?>
+</div>
+<div class="db-hash-header">
+	<div class="container">
+		<h1>Flexible Consulating</h1>
+	</div>
+</div>
+<div class="row">
+	<div class="col-md-9">
+		<p>We are<strong> very strong in MySQL and its related technologies</strong>.Our expertise include Installation,configuration,Query Tuning,Schema Design,Performance Optimization,Implementing HA,Setting up replication etc. We help you in resolving issues varying from simple query tuning to complex performance issues.</p>
+		<p>Flexible Consulating <strong>does not require any commitment.</strong> You can contact us whenever you need.Py us only for the hours we worked for you. We also have<strong> fixed price</strong> model for certain tasks like installing and configuring MySQL, setting up replication, basic performance tuning etc.You can choose any of these models, only <strong>minimum of these two models </strong>will be billed.</p>
+		<p>Please raise a <a href="#">ticket</a> for us breifly describing about the issue.Our <strong>SLA for P1 tickets is 15 minutes.</strong>If it is not urgent,please update the ticket with the time by which the task should be accomplished.<strong>We don't charge you based on priority.</strong></p>
+		<p class="align-center">
+			<a href="#"class="btn btn-outline-inverse btn-lg createticket" onclick="ga("send","event","Jumbotron actions")">CREATE TICKET</a>
+		</p>
+	</div>
+	<div class="db-hash-content">
+		<h4>Emergency Help Line</h4>
+		<table>
+			<tbody>
+				<tr>
+					<td><a href="skype:help.dbhash?call"><img src="http://www.dbhash.com/assets/icons/generic/rsz_skype.png" width="20" height="20"></a></td>
+					<td><a href="skype:help.dbhash?call">help.dbhash</a></td>
+				</tr>
+				<tr>
+					<td><a href="mailto:support@dbhash.com"><img src="http://dbhash.com/assets/icons/generic/email.png" width="20" height="20"></a></td>
+					<td><a href="mailto:support@dbhash.com"><b>support@dbhash.com</b></a></td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</div>
+
